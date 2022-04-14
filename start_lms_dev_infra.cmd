29420d697ecb2450322ce08b4cc148a9b04f86af
Subhrajit Sadhukhan <ssadhukhanv2.0@gmail.com>
2022-04-15 00:40:01 +0200

    delay creation of lms-ui until postgres starts(untracked file added)

---
+++ start_lms_dev_infra.cmd
@@ -0,0 +1,6 @@
+rmdir /s "./lms-databases/mongodb/data"
+rmdir /s "./lms-databases/mongodb/log"
+rmdir /s "./lms-databases/postgresql/data"
+rmdir /s "./lms-databases/postgresql/log"
+
+docker-compose --env-file .env -f docker-compose-dev-infra.yml up
